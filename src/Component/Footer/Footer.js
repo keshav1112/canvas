6db455091052418552bc0bcbc1984bc81f1da109
keshav1112 <keshav1112@gmail.com>
2021-02-07 22:18:59 +0530

    Delete src directory

--- src/Component/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer>
-      <p>Canvas8 Ltd © 2021</p>
-    </footer>
-  );
-};
-
-export default Footer;
--- src/Component/Layout/Loading/Loading.js
+++
@@ -1,5 +0,0 @@
-const Loading = () => {
-  return <div className="loading">Be Patience...</div>;
-};
-
-export default Loading;
